130354f5504a4228b0a0161a9a523921d1807994
Cleo <53199111+cleonyc@users.noreply.github.com>
2022-09-06 10:38:12 -0400

    Initial commit

---
+++ src/main/kotlin/nyc/cleo/changeme/ChangeMe.kt
@@ -0,0 +1,13 @@
+package nyc.cleo.changeme
+
+import net.axay.kspigot.main.KSpigot
+
+class ChangeMe : KSpigot() {
+    // onLoad()
+    override fun load() { }
+    // onEnable()
+    // Where most startup functionality should be
+    override fun startup() { }
+    // onDisable
+    override fun shutdown() { }
+}
